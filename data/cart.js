23a2fba285d445ac9662bf8d41f78a6e7a5f05f1
Rico.TechDev <151479600+rico-td@users.noreply.github.com>
2024-05-12 21:12:55 +0200

    save cart in local storage

--- data/cart.js
+++ data/cart.js
@@ -1,15 +1,8 @@
-export const cart = [{
-	productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-	quantity: 2,
-},
-{
-	productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-	quantity: 1,
-},
-{
-	productId: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
-	quantity: 1,
-}]
+export const cart = localStorage.getItem('cart')? JSON.parse(localStorage.getItem('cart')) : [];
+
+export function saveCartInLocalStorage() {
+	localStorage.setItem('cart', JSON.stringify(cart))	
+}
 
 export function addToCart(productId) {
 
@@ -37,7 +30,8 @@ export function addToCart(productId) {
 		});
 		quantitySelector.value = 1
 	}
-	return quantity
+	saveCartInLocalStorage()
+
 }
 
 export function removeFromCart(productId) {
@@ -46,5 +40,6 @@ export function removeFromCart(productId) {
 			cart.splice(cart.indexOf(item), 1);
 		}
 	})
+	saveCartInLocalStorage()
 }
 
