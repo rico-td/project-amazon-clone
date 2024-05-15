7593e38512cb2e1d27289fbd2650ae0e889b6f26
Rico.TechDev <151479600+rico-td@users.noreply.github.com>
2024-05-15 19:46:22 +0200

    add moneyTest.js

---
+++ tests-jasmine/spec/moneyTest.js
@@ -0,0 +1,20 @@
+import { formatCurrency } from '../../utils/money.js';
+
+describe('test suit: formatCurrency', () => {
+
+    // basic test
+    it('should return a string with with two decimals', () => {
+        expect(formatCurrency(1050)).toBe('10.50');
+    }); 
+
+    // zero test
+    it('should run with 0', () => {
+        expect(formatCurrency(0)).toBe('0.00');
+    }); 
+    it('should round up to the nearest cent with >= 0.5', () => {
+        expect(formatCurrency(1000.5)).toBe('10.01');
+    });
+    it('should round down to the nearest cent with < 0.5', () => {
+        expect(formatCurrency(1000.4)).toBe('10.00');
+    });
+});
