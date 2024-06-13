7d5b92bb9b06c7e50694559fa13981e59cb49f5d
Rico.TechDev <151479600+rico-td@users.noreply.github.com>
2024-06-13 22:52:41 +0200

    create order using the backend

---
+++ data/order.js
@@ -0,0 +1,11 @@
+export const orders = localStorage.getItem('orders') ? JSON.parse(localStorage.getItem('orders')) : [];  
+
+
+export function addOrder(order){
+    orders.unshift(order);
+    saveToStorage();
+}
+
+function saveToStorage(){
+    localStorage.setItem('orders', JSON.stringify(orders));
+}
--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -2,7 +2,7 @@ import {cart, getCartQuantity} from '../../data/cart.js';
 import {getProduct} from '../../data/products.js';
 import {getDeliveryOption} from '../../data/deliveryOptions.js';
 import {formatCurrency} from '../../utils/money.js';
-
+import { addOrder } from '../../data/order.js';
 
 export function renderPaymentSummary() {
   let productPriceCents = 0;
@@ -60,11 +60,37 @@ export function renderPaymentSummary() {
       </div>
     </div>
 
-    <button class="place-order-button button-primary">
+    <button class="js-place-order place-order-button button-primary">
       Place your order
     </button>
   `;
 
   document.querySelector('.js-payment-summary')
     .innerHTML = paymentSummaryHTML;
+
+  document.querySelector('.js-place-order')
+    .addEventListener('click', async () => {
+
+      try {
+
+      const response = await fetch('https://supersimplebackend.dev/orders', {
+        method: 'POST', 
+        headers: {
+          'content-type': 'application/json'  
+        },
+        body: JSON.stringify({  
+          cart: cart
+        })
+      })
+
+      const order = await response.json()
+      addOrder(order);
+
+    } catch (error) { 
+        console.log('error in handling the order placement');
+    }
+
+      window.location.href = 'orders.html';
+
+  });
 }
